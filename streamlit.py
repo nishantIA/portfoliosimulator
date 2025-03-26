06ff98374983b8c77e382aa04f2a576ec86f0e5c
Andrew Chan <70087509+chandr3w@users.noreply.github.com>
2025-03-25 18:49:33 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,6 +13,8 @@ import seaborn as sns
 # App Title
 st.markdown('<a href="https://atas.vc/"><img src="https://atas.vc/img/logo.png" width="150"></a>', unsafe_allow_html=True)
 st.title('Venture Capital Fund Simulator')
+st.set_page_config(page_title="Venture Capital Fund Simulator", page_icon="💼")
+
 st.markdown(
     "This open source model was developed by [Andrew Chan](https://www.linkedin.com/in/chandr3w/) "
     "from [Atas VC](https://atas.vc/) as an open source resource to help push the venture capital into a future, data-driven era."
