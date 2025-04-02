4d5969d5e989a1109a1de6c19182b149f60e1644
Andrew Chan <70087509+chandr3w@users.noreply.github.com>
2025-04-02 13:36:51 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,7 +16,7 @@ import numpy_financial as npf
 st.set_page_config(page_title="VC Fund Simulator", page_icon="https://atas.vc/img/favicon.png")
 st.markdown('<a href="https://atas.vc/"><img src="https://atas.vc/img/logo.png" width="150"></a>', unsafe_allow_html=True)
 st.markdown(
-    "This open source model was developed by [Andrew Chan](https://www.linkedin.com/in/chandr3w/), "
+    "This open source model was developed by [Andrew Chan](https://www.linkedin.com/in/chandr3w/) "
     "from [Atas VC](https://atas.vc/)."
 )
 
